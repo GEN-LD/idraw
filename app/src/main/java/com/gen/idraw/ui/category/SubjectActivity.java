7c4c501d2a9df6e2cfd7e459ea6b7eb063d94945
Liangdg <1138421194@qq.com>
2026-06-09 13:52:27 +0800

    refactor: 缩小类别选择页面UI尺寸，移除线稿功能
    - 类别图标从120dp改为60dp
    
    - 类别文字从22sp改为16sp
    
    - 进入画画页面不再显示线稿

--- app/src/main/java/com/gen/idraw/ui/category/SubjectActivity.java
+++ app/src/main/java/com/gen/idraw/ui/category/SubjectActivity.java
@@ -47,7 +47,6 @@ public class SubjectActivity extends AppCompatActivity {
 
     private void openDrawing(DrawingSubject subject) {
         Intent intent = new Intent(this, DrawingActivity.class);
-        intent.putExtra(DrawingActivity.EXTRA_LINE_ART_RES_ID, subject.getLineArtResId());
         startActivity(intent);
     }
 
